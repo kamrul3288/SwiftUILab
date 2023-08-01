63389e8a2582d0e386bee55b403678d524b65926
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-08-01 11:23:22 +0600

    Initial Commit

---
+++ SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by Kamrul Hasan on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
